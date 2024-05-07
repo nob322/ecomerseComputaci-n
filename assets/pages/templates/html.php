4b27c029a11887171186609c2302a04239969c5d
delgados-coder <delgados.coder@gmail.com>
2024-05-06 23:25:55 -0300

    primera actualizacion commit

---
+++ assets/pages/templates/html.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<?php
+
+require("head.php");
+require("body.php");
+
+?>
+
+    
+</html>
---
+++ assets/pages/templates/main.php
@@ -0,0 +1,8 @@
+<main class="main">
+            <section class="banner">Banner</section>
+            <section class="container">
+                <button class="btn btn-primary">HOla</button>
+
+            </section>
+            
+</main>
