400013e5f94a053c3a037b6381288d3872a46587
delgados-coder <delgados.coder@gmail.com>
2024-05-08 19:40:41 -0300

    Resubi la estructura para adptarlos al modelo Creado por AgusXpunks

--- assets/pages/templates/main.php
+++
@@ -1,8 +0,0 @@
-<main class="main">
-            <section class="banner">Banner</section>
-            <section class="container">
-                <button class="btn btn-primary">HOla</button>
-
-            </section>
-            
-</main>
